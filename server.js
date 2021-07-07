fa196e922c1c70c0315e0412ddab0ff8e8925200
Hussein Saad <husseinsaad247@gmail.com>
2021-07-07 09:14:03 -0400

    Remove ejs view engine

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 require('dotenv').config();
 
 const app = express();
-
-app.set("view engine", "ejs");
 
 app.use(
   bodyParser.urlencoded({
